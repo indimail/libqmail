4c1e752cebf6c4f7f379c09a5d6b82b568157343
Manvendra Bhangui <mbhangui@gmail.com>
2019-03-21 16:14:56 +0530

    added scan_plusmins()

--- scan.h
+++ scan.h
@@ -11,25 +11,13 @@
 #define SCAN_H
 
 unsigned int    scan_uint(char *, unsigned int *);
-unsigned int    scan_xint(char *, unsigned int *);
-unsigned int    scan_nbbint(char *, unsigned int, unsigned int, unsigned int, unsigned int *);
 unsigned int    scan_ushort(char *, unsigned short *);
-unsigned int    scan_xshort(char *, unsigned short *);
-unsigned int    scan_nbbshort(char *, unsigned int, unsigned int, unsigned int, unsigned short *);
 unsigned int    scan_ulong(char *, unsigned long *);
 unsigned int    scan_ulonglong(char *, unsigned long long *);
 unsigned int    scan_xlong(char *, unsigned long *);
-unsigned int    scan_nbblong(char *, unsigned int, unsigned int, unsigned int, unsigned long *);
-unsigned int    scan_plusminus(char *, int *);
-unsigned int    scan_0x(char *, unsigned int *);
-unsigned int    scan_whitenskip(char *, unsigned int);
-unsigned int    scan_nonwhitenskip(char *, unsigned int);
-unsigned int    scan_charsetnskip(char *, char *, unsigned int);
-unsigned int    scan_noncharsetnskip(char *, char *, unsigned int);
-unsigned int    scan_strncmp(char *, char *, unsigned int);
-unsigned int    scan_memcmp(char *, char *, unsigned int);
 unsigned int    scan_long(char *, long *);
-unsigned int    scan_8long(char *s, unsigned long *u);
-unsigned int    scan_int(char *s, int *i);
+unsigned int    scan_8long(char *, unsigned long *);
+unsigned int    scan_int(char *, int *);
+unsigned int    scan_plusminus(char *, int *);
 
 #endif
