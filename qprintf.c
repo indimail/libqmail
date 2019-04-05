b94389e2c3b4a1ead12743ecb1c24f47317c17bf
Manvendra Bhangui <mbhangui@gmail.com>
2019-04-05 14:14:18 +0530

    made substdio configurable

--- qprintf.c
+++ qprintf.c
@@ -2,34 +2,44 @@
  * $Log: $
  */
 #include "substdio.h"
-#include "subfd.h"
 #include "scan.h"
 #include "str.h"
 #include "strerr.h"
 
 int
-qprintf(char *src, char *format_spec)
+qprintf_flush(substdio *ss)
+{
+	if (substdio_flush(ss))
+		strerr_die1x(111, "qprintf: unable to write output: ");
+	return (0);
+}
+
+int
+qprintf(substdio *ss, char *src, char *format_spec)
 {
 	register int    len = 0, todo;
 	int             i = -1;
 
 	len = str_len(src);
-	if (*format_spec != '%')
-		return (substdio_put(subfdoutsmall, src, len));
+	if (*format_spec != '%') {
+		if (substdio_put(ss, src, len) == -1)
+			strerr_die1x(111, "qprintf: unable to write output: ");
+		return (0);
+	}
 	switch (*(format_spec + 1))
 	{
 	case '+':
 		scan_int(format_spec + 2, &i);
 		if ((todo = i - len) > 0) {
 			for (; todo > 0; todo--) {
-				if (substdio_put(subfdoutsmall, " ", 1) == -1)
+				if (substdio_put(ss, " ", 1) == -1)
 					strerr_die1x(111, "qprintf: unable to write output: ");
 			}
-			if (substdio_put(subfdoutsmall, src, len) == -1)
+			if (substdio_put(ss, src, len) == -1)
 				strerr_die1x(111, "qprintf: unable to write output: ");
 			return (0);
 		} else
-			return (substdio_put(subfdoutsmall, src, len));
+			return (substdio_put(ss, src, len));
 		break;
 	case '-':
 		scan_int(format_spec + 2, &i);
@@ -37,15 +47,18 @@ qprintf(char *src, char *format_spec)
 		if (i == -1)
 			scan_int(format_spec + 1, &i);
 		if (len < i) {
-			if (substdio_put(subfdoutsmall, src, len) == -1)
+			if (substdio_put(ss, src, len) == -1)
 				strerr_die1x(111, "qprintf: unable to write output: ");
 			for (todo = i - len; todo; todo--) {
-				if (substdio_put(subfdoutsmall, " ", 1) == -1)
+				if (substdio_put(ss, " ", 1) == -1)
 					strerr_die1x(111, "qprintf: unable to write output: ");
 			}
 			return (0);
-		} else
-			return (substdio_put(subfdoutsmall, src, len));
+		} else {
+			if (substdio_put(ss, src, len) == -1)
+				strerr_die1x(111, "qprintf: unable to write output: ");
+			return (0);
+		}
 		break;
 	}
 }
