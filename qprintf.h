9db5384794ed790b9e1286113630f98cb6c90158
Manvendra Bhangui <mbhangui@gmail.com>
2019-04-05 14:16:26 +0530

    added qprintf function

---
+++ qprintf.h
@@ -0,0 +1,10 @@
+/*
+ * $Log: $
+ */
+#ifndef QPRINTF_H
+#define QPRINTF_H
+#include "substdio.h"
+
+int             qprintf(substdio *, char *, char *);
+int             qprintf_flush(substdio *);
+#endif
