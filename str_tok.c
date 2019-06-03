a36f11de6f3a6fc2e21050ea784931624a4dd478
Manvendra Bhangui <mbhangui@gmail.com>
2019-06-03 21:32:56 +0530

    added str_tok() function

---
+++ str_tok.c
@@ -0,0 +1,69 @@
+/* 
+ * str_tok.c --
+ *
+ *	Source code for the "str_tok" library routine.
+ *
+ * Copyright 1988 Regents of the University of California
+ * Permission to use, copy, modify, and distribute this
+ * software and its documentation for any purpose and without
+ * fee is hereby granted, provided that the above copyright
+ * notice appear in all copies.  The University of California
+ * makes no representations about the suitability of this
+ * software for any purpose.  It is provided "as is" without
+ * express or implied warranty.
+ */
+
+#include "str.h"
+
+static char *
+local_strchr (register const char *s, int c)
+{
+  do {
+    if (*s == c)
+      {
+        return (char*)s;
+      }
+  } while (*s++);
+  return (0);
+}
+
+/*
+ *----------------------------------------------------------------------
+ *
+ * str_tok --
+ *
+ *  	Split a string up into tokens
+ *
+ * Results:
+ *      If the first argument is non-NULL then a pointer to the
+ *      first token in the string is returned.  Otherwise the
+ *      next token of the previous string is returned.  If there
+ *      are no more tokens, NULL is returned.
+ *
+ * Side effects:
+ *	Overwrites the delimiting character at the end of each token
+ *      with '\0'.
+ *
+ *----------------------------------------------------------------------
+ */
+
+char *
+str_tok(s, delim)
+    char *s;            /* string to search for tokens */
+    const char *delim;  /* delimiting characters */
+{
+    static char *lasts;
+    register int ch;
+
+    if (s == 0)
+	s = lasts;
+    do {
+	if ((ch = *s++) == '\0')
+	    return 0;
+    } while (local_strchr(delim, ch));
+    --s;
+    lasts = s + str_cspn(s, delim);
+    if (*lasts != 0)
+	*lasts++ = 0;
+    return s;
+}
