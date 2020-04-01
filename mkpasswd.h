b0d276967699f8836725d00845e0c1e98920f4f3
mbhangui <mbhangui@gmail.com>
2020-04-01 18:15:13 +0530

    proto for mkpasswd() function

---
+++ mkpasswd.h
@@ -0,0 +1,12 @@
+/*
+ * $Log: mkpasswd.h,v $
+ * Revision 1.1  2020-04-01 18:14:56+05:30  Cprogrammer
+ * Initial revision
+ *
+ */
+#ifndef MKPASSWD_H
+#define MKPASSWD_H
+
+int             mkpasswd(char *, stralloc *, int);
+
+#endif
