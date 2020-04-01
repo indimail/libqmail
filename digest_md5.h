09aba2a9ba4e8122f8820f8e82d68d8e2f9ccf20
mbhangui <mbhangui@gmail.com>
2020-04-01 17:58:34 +0530

    header for digest_md5()

---
+++ digest_md5.h
@@ -0,0 +1,15 @@
+/*
+ * $Log: digest_md5.h,v $
+ * Revision 1.1  2020-04-01 17:58:06+05:30  Cprogrammer
+ * Initial revision
+ *
+ * Revision 1.1  2019-04-13 23:39:26+05:30  Cprogrammer
+ * digest_md5.h
+ *
+ */
+#ifndef DIGEST_MD5_H
+#define DIGEST_MD5_H
+
+int             digest_md5(char *, unsigned char *, unsigned char *, unsigned char *);
+
+#endif
