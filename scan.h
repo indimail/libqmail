a2a957c079a4f364ee9883371308bee2368e5d8a
Manvendra Bhangui <mbhangui@gmail.com>
2020-07-23 11:31:05 +0530

    added scan_double(), scan_hexdump() prototyps

--- scan.h
+++ scan.h
@@ -1,5 +1,8 @@
 /*
  * $Log: scan.h,v $
+ * Revision 1.8  2020-07-23 11:27:48+05:30  Cprogrammer
+ * added scan_double(), scan_hexdump() prototyps
+ *
  * Revision 1.7  2020-07-01 12:14:40+05:30  Cprogrammer
  * added scan_fromhex()
  *
@@ -16,6 +19,15 @@
 #ifndef SCAN_H
 #define SCAN_H
 
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#else
+#include <sys/types.h>
+#endif
+#ifdef HAVE_SYS_TYPES_H
+#include <sys/types.h>
+#endif
+
 unsigned int    scan_uint(char *, unsigned int *);
 unsigned int    scan_ushort(char *, unsigned short *);
 unsigned int    scan_ulong(char *, unsigned long *);
@@ -26,5 +38,7 @@ unsigned int    scan_8long(char *, unsigned long *);
 unsigned int    scan_int(char *, int *);
 unsigned int    scan_plusminus(char *, int *);
 int             scan_fromhex(unsigned char c);
+unsigned int    scan_double(const char *, double *);
+size_t          scan_hexdump(const char *, char *, size_t *);
 
 #endif
