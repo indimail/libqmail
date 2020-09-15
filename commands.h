d2e3a91c8887175047f11962372f6356e829e7aa
Manvendra Bhangui <mbhangui@gmail.com>
2020-09-15 10:15:38 +0530

    added mess822, commands, rewritehost, sconfig functions from indimail-mta

---
+++ commands.h
@@ -0,0 +1,29 @@
+/*
+ * $Log: commands.h,v $
+ * Revision 1.4  2020-09-15 10:00:39+05:30  Cprogrammer
+ * added ctl_maxcmdlen variable in commands.c
+ *
+ * Revision 1.3  2004-10-11 13:51:32+05:30  Cprogrammer
+ * added function prototypes
+ *
+ * Revision 1.2  2004-06-18 22:58:14+05:30  Cprogrammer
+ * added RCS log
+ *
+ */
+#ifndef COMMANDS_H
+#define COMMANDS_H
+#include "substdio.h"
+
+struct commands
+{
+	char           *text;
+	void            (*fun) ();
+	void            (*flush) ();
+};
+#ifndef COMMANDS_C
+extern int      ctl_maxcmdlen;
+#endif
+
+int             commands(substdio *, struct commands *);
+
+#endif
