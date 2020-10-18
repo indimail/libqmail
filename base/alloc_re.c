601cbfb81087038011859f9f3949df11d9f12d85
Manvendra Bhangui <mbhangui@gmail.com>
2020-10-18 09:29:24 +0530

    fix overwriting unallocate memory location when new_size < old_size

--- base/alloc_re.c
+++ base/alloc_re.c
@@ -1,5 +1,8 @@
 /*
  * $Log: alloc_re.c,v $
+ * Revision 1.6  2020-10-18 09:28:24+05:30  Cprogrammer
+ * fix overwriting unallocate memory location when new_size < old_size
+ *
  * Revision 1.5  2004-10-22 20:18:06+05:30  Cprogrammer
  * added RCS id
  *
@@ -23,11 +26,13 @@ alloc_re(x, old_size, new_size)
 {
 	char           *y;
 
-	y = alloc(new_size);
-	if (!y)
+	if (old_size == new_size)
+		return 1;
+	if (!(y = alloc(new_size)))
 		return 0;
-	byte_copy(y, old_size, *x);
-	alloc_free(*x);
+	byte_copy(y, new_size < old_size ? new_size : old_size, *x);
+	if (*x)
+		alloc_free(*x);
 	*x = y;
 	return 1;
 }
@@ -35,7 +40,7 @@ alloc_re(x, old_size, new_size)
 void
 getversion_alloc_re_c()
 {
-	static char    *x = "$Id: alloc_re.c,v 1.5 2004-10-22 20:18:06+05:30 Cprogrammer Stab mbhangui $";
+	static char    *x = "$Id: alloc_re.c,v 1.6 2020-10-18 09:28:24+05:30 Cprogrammer Exp mbhangui $";
 
 	x++;
 }
