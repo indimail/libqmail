a53cc954f6a9f340860f93784254e4d398bf1281
Manvendra Bhangui <mbhangui@gmail.com>
2021-05-13 12:23:08 +0530

    added RCS log

--- base/timestamp.h
+++ base/timestamp.h
@@ -1,5 +1,8 @@
 /*
  * $Log: timestamp.h,v $
+ * Revision 1.2  2021-05-13 09:38:41+05:30  Cprogrammer
+ * fixed compiler warning
+ *
  * Revision 1.1  2004-09-19 18:54:21+05:30  Cprogrammer
  * Initial revision
  *
