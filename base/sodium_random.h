51587feca80c73929e8bd4bb3cc5e1e6b801413f
Manvendra Bhangui <mbhangui@gmail.com>
2022-08-05 09:06:41 +0530

    added stdint.h if config.h is not included

--- base/sodium_random.h
+++ base/sodium_random.h
@@ -1,6 +1,6 @@
 /*
  * $Log: sodium_random.h,v $
- * Revision 1.1  2022-08-05 09:00:02+05:30  Cprogrammer
+ * Revision 1.1  2022-08-05 09:06:12+05:30  Cprogrammer
  * Initial revision
  *
  */
@@ -10,9 +10,13 @@
 #include "config.h"
 #else
 #include <sys/types.h>
+#include <stdint.h>
 #endif
 #ifdef HAVE_SYS_TYPES_H
 #include <sys/types.h>
+#endif
+#ifdef HAVE_STDINT_H
+#include <stdint.h>
 #endif
 
 uint32_t        sodium_random(char *, size_t);
