6c23d06586c6a35fc202d769bc2d1dbe32babf27
Manvendra Bhangui <mbhangui@gmail.com>
2022-08-25 12:08:14 +0530

    added hexsaltedpw, cleartxt fields

--- base/get_scram_secrets.h
+++ base/get_scram_secrets.h
@@ -1,5 +1,8 @@
 /*
  * $Log: get_scram_secrets.h,v $
+ * Revision 1.2  2022-08-25 12:05:11+05:30  Cprogrammer
+ * added hexsaltedpw, cleartxt fields
+ *
  * Revision 1.1  2022-08-04 10:01:03+05:30  Cprogrammer
  * Initial revision
  *
@@ -8,6 +11,6 @@
 #ifndef _GET_SECRETS_H
 #define _GET_SECRETS_H
 
-int             get_scram_secrets(char *, int *, int *, char **, char **, char **, char **);
+int             get_scram_secrets(char *, int *, int *, char **, char **, char **, char **, char **, char **);
 
 #endif
