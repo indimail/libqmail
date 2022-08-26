a4cd5ef0513e978d4331d6c08f0682fd869c720a
Manvendra Bhangui <mbhangui@gmail.com>
2022-08-26 18:40:44 +0530

    added valid_passwd_chars function

---
+++ base/valid_password_chars.c
@@ -0,0 +1,33 @@
+/*
+ * $Log: valid_password_chars.c,v $
+ * Revision 1.1  2022-08-26 18:33:05+05:30  Cprogrammer
+ * Initial revision
+ *
+ */
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+#include "makesalt.h"
+#include "str.h"
+
+int
+valid_password_chars(char *password)
+{
+	int             i;
+	char           *ptr;
+
+	for (ptr = password; *ptr; ptr++) {
+		i = str_chr(pchars, *ptr);
+		if (!pchars[i])
+			return 1;
+	}
+	return 0;	
+}
+
+void
+getversion_valid_pasword_chars_c()
+{
+	static char    *x = "$Id: valid_password_chars.c,v 1.1 2022-08-26 18:33:05+05:30 Cprogrammer Exp mbhangui $";
+
+	x++;
+}
---
+++ base/valid_password_chars.h
@@ -0,0 +1,12 @@
+/*
+ * $Log: valid_password_chars.h,v $
+ * Revision 1.1  2022-08-26 18:08:12+05:30  Cprogrammer
+ * Initial revision
+ *
+ */
+#ifndef _VALID_PASSWD_CHARS_H
+#define _VALID_PASSWD_CHARS_H
+
+int             valid_password_chars(char *);
+
+#endif
